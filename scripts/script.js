d3abaf170d0e134185ee878a32a41e356a48dc78
avizmarques <aviz.marques@gmail.com>
2019-12-11 16:23:35 +0100

    added script for responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -8,12 +8,63 @@ window.onscroll = function() {
     }
 }
 
-window.onresize = function() {
-    let threeColumns = document.querySelector('#favourite-items');
+function changeFlexDirection(id) {
+    let flexItem = document.querySelector(id);
 
     if (window.innerWidth < 750) {
-        threeColumns.className = 'flex-column';
+        flexItem.className = 'flex-column';
     } else {
-        threeColumns.className = 'three-columns';
+        flexItem.className = 'flex-row';
     }
+}
+
+function changePineapple(id) {
+    let pineapple = document.querySelector(id);
+
+    if (window.innerWidth < 550) {
+        pineapple.className = 'resized-pineapple';
+    } else {
+        pineapple.className = 'pineapple';
+    }
+}
+
+function toggleTextStyle(id) {
+    let text = document.querySelector(id);
+
+    if (window.innerWidth < 750) {
+        text.style.left = 0;
+        text.style.padding = 0;
+    } else {
+        text.style.left = '-70px';
+        text.style.padding = '40px';
+    }
+}
+
+function changeImageSize(id) {
+    let img = document.querySelector(id);
+
+    if (window.innerWidth < 750) {
+        img.style.width = '100%';
+        img.style.maxWidth = '350px';
+        img.style.padding = '4vh 0';
+    } else {
+        img.style.width = '50%';
+        img.style.maxWidth = '';
+        img.style.padding = '';
+    }
+}
+
+function changesOnResize() {
+    changeFlexDirection('#favourite-things');
+    changeFlexDirection('#about');
+    changeFlexDirection('#games');
+    changePineapple('#big-pineapple');
+    toggleTextStyle('#text-games');
+    changeImageSize('#game-pic');
+}
+
+changesOnResize();
+
+window.onresize = function() {
+ this.changesOnResize();
 }
